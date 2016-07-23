2081fbd273a7d3a2c7b09bdf1e022bac1bc0f03e
jeromechan <jerome.chan369@gmail.com>
2016-07-23 13:42:58 +0800

    update

--- jekyll-deploy.sh
+++ jekyll-deploy.sh
@@ -49,7 +49,7 @@ last_SHA=( $(git log -n 1 --pretty=oneline) )
 tmp_dir="temp_$last_SHA"
 echo $tmp_dir
 # Build the Jekyll site directly to a temporary folder
-jekyll build -d ~/$tmp_dir > /dev/null 2>&1
+jekyll build -d $tmp_dir > /dev/null 2>&1
 
 if [ $? = 0 ]; then
   echo "Jekyll build successful"
@@ -71,9 +71,10 @@ fi
 # Remove the current contents of the SITE branch and
 #   replace them with the contents of the temp folder
 current_dir=${PWD}
+echo "The contents of $current_dir will be remove"
 rm -r $current_dir/*
 git rm -r --cached * > /dev/null 2>&1
-cp -r ~/$tmp_dir/* $current_dir
+cp -r $tmp_dir/* $current_dir
 
 # Commit the changes to the SITE branch
 message="Automated update $SITE site from $SOURCE ($last_SHA) by jeromechan"
@@ -83,7 +84,7 @@ git add --all .
 git commit -m "$message" > /dev/null 2>&1
 
 # Delete the temporary folder
-rm -r ~/$tmp_dir
+rm -r $tmp_dir
 
 # Push latest SITE to server
 git push -u origin $SITE > /dev/null 2>&1
