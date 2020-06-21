bcb8b8e7b836dec0c14dbd84cc2a154b6cd4efc3
jeromechan <jerome.chan369@gmail.com>
2020-06-21 23:32:15 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "执行开始 \n"
+
 # A script to build a Hexo site
 REPO_NAME="jeromechan.github.io"
 BRANCH_SOURCE="source-hexo"
@@ -7,34 +9,42 @@ BRANCH_MASTER="master"
 CURRENT_DIR=${PWD}
 
 # 1. 切换至source-hexo分支下，执行hexo clean & hexo generate
+echo "1. 切换至source-hexo分支下，执行hexo clean & hexo generate \n"
 git checkout $BRANCH_SOURCE
 hexo clean
 hexo generate
 
 # 2. 执行git add * & git commit -am 'Auto update file using hexo-deploy.sh script.' & git push
+echo "2. 执行git add * & git commit -am 'Auto update file using hexo-deploy.sh script.' & git push \n"
 git add *
 git commit -am 'Auto update file using hexo-deploy.sh script.'
 git push
 
 # 3. 将public文件夹内容拷贝至/tmp/jeromechan.github.io，额外包含项目目录下的"CNAME"文件
-#mkdir $REPO_NAME > /dev/null 2>&1
+echo "3. 将public文件夹内容拷贝至/tmp/jeromechan.github.io，额外包含项目目录下的"CNAME"文件 \n"
 rm -rf /tmp/$REPO_NAME
 cp -r ./public/ /tmp/$REPO_NAME
 cp ./CNAME /tmp/$REPO_NAME/
 
 # 4. 切换至master分支下，删除所有非隐藏的项目文件，然后执行git commit -am 'Auto reset master files using hexo-deploy.sh script.'
+echo "4. 切换至master分支下，删除所有非隐藏的项目文件，然后执行git commit -am 'Auto reset master files using hexo-deploy.sh script.' \n"
 git checkout $BRANCH_MASTER
 rm -r $CURRENT_DIR/*
 git commit -am 'Auto reset master files using hexo-deploy.sh script.'
 
 # 5. 将/tmp/jeromechan.github.io文件内容全部拷贝至master分支下
+echo "5. 将/tmp/jeromechan.github.io文件内容全部拷贝至master分支下 \n"
 cp -r /tmp/$REPO_NAME/* ./
 rm -rf /tmp/$REPO_NAME
 
 # 6. 执行git add * & git commit -am 'Refresh and add master files using hexo-deploy.sh script.' & git push
+echo "6. 执行git add * & git commit -am 'Refresh and add master files using hexo-deploy.sh script.' & git push \n"
 git add *
 git commit -am 'Refresh and add master files using hexo-deploy.sh script.'
 git push
 
 # 7. 切换至source-hexo分支下
+echo "7. 切换至source-hexo分支下 \n"
 git checkout $BRANCH_SOURCE
+
+echo "执行完成 \n"
